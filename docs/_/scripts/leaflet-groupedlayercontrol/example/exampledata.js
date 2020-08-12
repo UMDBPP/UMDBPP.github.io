6aff185aec507bca77e012ba9972e722cd7e44f8
Zach <zrb@umd.edu>
2020-08-12 11:43:46 -0400

    change directory name to match GitHub Pages content root

--- docs/_/scripts/leaflet-groupedlayercontrol/example/exampledata.js
+++ docs/_/scripts/leaflet-groupedlayercontrol/example/exampledata.js
@@ -2,9 +2,9 @@
 
     var basemaps = {
         Grayscale: L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-            maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            maxZoom: 18, attribution: '&copy; <a href="http://docs.openstreetmap.org/copyright">OpenStreetMap</a>'
         }), Streets: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            maxZoom: 19, attribution: '&copy; <a href="http://docs.openstreetmap.org/copyright">OpenStreetMap</a>'
         })
     };
 
